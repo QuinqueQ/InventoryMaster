66448266ef7af47d73153e8f3daaecbc4bc902af
Nazar <loqixck@gmail.com>
2024-01-31 21:26:17 +0300

    changes

--- InventoryMaster/InventoryMaster/Model/Item.cs
+++ InventoryMaster/InventoryMaster/Model/Item.cs
@@ -1,15 +1,24 @@
 ﻿using System.Text.Json.Serialization;
-
+using System.Linq;
 namespace InventoryMaster.Model
 {
     public class Item
     {
-        public int Id { get; set; }
+        public Item()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
         public string Name { get; set; }
+        public int count { get; set; } = 0;
 
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public TypesOFItems Type { get; set; }
         public double Price { get; set; }
 
+
+
+        
     }
+
 }
